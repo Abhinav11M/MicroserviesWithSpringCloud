849f498d6f9c3101eed4f9490a376d1936ce069d
Abhinav <aabhinav11m@gmail.com>
2020-06-02 00:57:48 +0530

    Finished till level 2.

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -1,44 +1,69 @@
 package io.javabrains.moviecatalogservice.resources;
 
-import io.javabrains.moviecatalogservice.models.CatalogItem;
-import io.javabrains.moviecatalogservice.models.Movie;
-import io.javabrains.moviecatalogservice.models.Rating;
-import io.javabrains.moviecatalogservice.models.UserRating;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.UserRating;
+import services.MovieInfo;
+import services.UserRatingsInfo;
 
 @RestController
 @RequestMapping("/catalog")
+@ComponentScan("services")
 public class CatalogResource {
 
-    @Autowired
-    private RestTemplate restTemplate;
+//    @Autowired
+//    private RestTemplate restTemplate;
 
+//    @Autowired
+//    WebClient.Builder webClientBuilder;
+    
     @Autowired
-    WebClient.Builder webClientBuilder;
+    private UserRatingsInfo ratingInfo;
+    
+    @Autowired
+    private MovieInfo movieInfo;
 
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+    	UserRating userRating = ratingInfo.getUserRating(userId);
+//		UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/user/" + userId,
+//				UserRating.class);
 
-        return userRating.getRatings().stream()
-                .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
-                })
-                .collect(Collectors.toList());
+		return userRating.getRatings().stream()
+				.map(x -> movieInfo.getCatalogItem(x))
+				.collect(Collectors.toList());
+	}
 
-    }
+//    @HystrixCommand(fallbackMethod = "getFallBackUserRating")
+//	public UserRating getUserRating(String userId) {
+//    	return restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+//	}
+	
+//    @HystrixCommand(fallbackMethod = "getFallBackCatalogItem")
+//	public CatalogItem getCatalogItem(Rating rating) {
+//          Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+////			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+//		
+//          return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+//	}
+
+//	public UserRating getFallBackUserRating(String userId) {
+//		return new UserRating(userId, Arrays.asList(new Rating("0", 0)));
+//	}
+
+//	public CatalogItem getFallBackCatalogItem(Rating rating) {
+//		return new CatalogItem("Movie name not found", "", rating.getRating());
+//	}
 }
 
 /*
--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
@@ -1,13 +1,14 @@
 package io.javabrains.movieinfoservice.resources;
 
-import io.javabrains.movieinfoservice.models.Movie;
-import io.javabrains.movieinfoservice.models.MovieSummary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import io.javabrains.movieinfoservice.models.Movie;
+import io.javabrains.movieinfoservice.models.MovieSummary;
 
 @RestController
 @RequestMapping("/movies")
@@ -22,8 +23,8 @@ public class MovieResource {
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
         MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" +  apiKey, MovieSummary.class);
-        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
-
+        return new Movie(movieId, movieSummary.getOriginal_title(), movieSummary.getOverview());
+//    	return new Movie(movieId, "Transformere", "Revenge of the fallen");
     }
 
 }
